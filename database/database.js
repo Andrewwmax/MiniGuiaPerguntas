e04ee20654cad985553915fc8026cf0f8d6dfd26
Andrewwmax <andreww.max@hotmail.com>
2020-11-26 12:49:21 -0300

    Revert "Subindo os códigos"
    This reverts commit e7b7224033293d845fead522b4b10469fd58200e.

--- database/database.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize')
-
-const connection = new Sequelize('guiaperguntas', 'root', 'AndrewwMax', {
-    host: 'localhost',
-    dialect: 'mysql'
-})
-
-module.exports = connection
